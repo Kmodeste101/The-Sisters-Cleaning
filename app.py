f400e8034d5eb78778d3408038f6363aa3315eaf
Kmodeste101 <149025335+Kmodeste101@users.noreply.github.com>
2023-10-28 03:27:38 +0000

    added a /booking page

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home():
   return render_template('home.html')
+  
+@app.route("/booking")
+def booking():
+    return render_template('booking.html')
   
 if__name__="__main__"
 app.run(host='0.0.0.0',debug=True)
