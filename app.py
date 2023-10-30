f4b09acf8321ffb03390081008fcf38dec3a7647
Kmodeste101 <149025335+Kmodeste101@users.noreply.github.com>
2023-10-30 05:58:50 +0000

    added service using for loop

--- app.py
+++ app.py
@@ -2,9 +2,26 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+SERVICES = [
+  {
+  'id': 1,
+  'title': 'Window Cleaning',
+  'description': 'Cleaning windows in the house',
+  'cost': '$50.00'
+  
+},
+  {'id':2,
+  'title':'Post-construction cleaning',
+  'description':'Cleaning windows in the house',
+  'cost':'$100.00'
+             
+  }
+]
+
 @app.route("/")
 def home():
-  return render_template('home.html')
+  return render_template('home.html',
+                         services = SERVICES)
   
 @app.route("/booking")
 def booking():
