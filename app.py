7a02dd9e9ba8de68861c67ab8f0b8755811a3424
Kmodeste101 <149025335+Kmodeste101@users.noreply.github.com>
2023-11-04 18:28:53 +0000

    added more services

--- app.py
+++ app.py
@@ -2,27 +2,34 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-SERVICES = [
-  {
-  'id': 1,
-  'title': 'Window Cleaning',
-  'description': 'Cleaning windows in the house',
-  'cost': '$50.00'
-  
-},
-  {'id':2,
-  'title':'Post-construction cleaning',
-  'description':'Cleaning windows in the house',
-  'cost':'$100.00'
-             
-  }
-]
+SERVICES = [{
+    'id': 1,
+    'title': 'Airbnb Cleaning',
+    'description': 'Cleaning windows in the house',
+    'cost': '$50.00'
+}, {
+    'id': 2,
+    'title': 'Post-construction cleaning',
+    'description': 'Cleaning windows in the house',
+    'cost': '$100.00'
+}, {
+    'id': 3,
+    'title': 'House cleaning',
+    'description': 'Cleaning windows in the house',
+    'cost': '$100.00'
+}, {
+    'id': 4,
+    'title': 'Move in/ Move out cleaning',
+    'description': 'Cleaning windows in the house',
+    'cost': '$100.00'
+}]
+
 
 @app.route("/")
 def home():
-  return render_template('home.html',
-                         services = SERVICES)
-  
+  return render_template('home.html', services=SERVICES)
+
+
 @app.route("/booking")
 def booking():
   return render_template('booking.html')
@@ -31,6 +38,7 @@ def booking():
 @app.route('/contact')
 def contact():
   return render_template('contact.html')
-  
-if__name__="__main__"
-app.run(host='0.0.0.0',debug=True)
+
+
+if__name__ = "__main__"
+app.run(host='0.0.0.0', debug=True)
